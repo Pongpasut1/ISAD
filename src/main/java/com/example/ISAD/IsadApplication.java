34e8b06029941696569f0208561e8a531238af5d
Pongpasut1 <66070122@kmitl.ac.th>
2024-09-24 20:47:00 +0700

    first commit

---
+++ src/main/java/com/example/ISAD/IsadApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ISAD/IsadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ISAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
