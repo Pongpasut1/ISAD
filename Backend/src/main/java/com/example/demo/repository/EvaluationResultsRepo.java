14b760232fffebd3b407b731cd6b5d0d7efc869a
Patlapa <141336257+66070149@users.noreply.github.com>
2024-10-07 22:21:28 +0700

    Update EvaluationResultsRepo.java

--- Backend/src/main/java/com/example/demo/repository/EvaluationResultsRepo.java
+++ Backend/src/main/java/com/example/demo/repository/EvaluationResultsRepo.java
@@ -2,11 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.model.EvaluationResults;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface EvaluationResultsRepo extends MongoRepository<EvaluationResults, Long> {
+    @Query(sort = "{ 'total_score': -1 }")
     List<EvaluationResults> findAllByOrderByTotalScoreDesc();
 }
